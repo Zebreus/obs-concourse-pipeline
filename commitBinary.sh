a6d3bc8b100426418a526f9b6b04e620ed68755a
Lennart Eichhorn <lennart@madmanfred.com>
2019-11-19 16:25:47 +0100

    Added pipeline and script for commiting binarys.

---
+++ commitBinary.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+set -e # fail fast
+set -x # print commands
+
+#git clone resource-gist updated-gist
+
+#cd updated-gist
+#date > bumpme
+
+git config --global user.email "lennart@madmanfred.com"
+git config --global user.name "build-bot"
+
+git add .
+git commit -m "Built new binary."
